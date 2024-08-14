6ca3a5ebf1fe7a2927f4e4b3d90930695f2d4ace
Roner <ronerumb@gmail.com>
2024-08-14 17:28:30 -0300

    inicio modelo controller

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -12,9 +12,15 @@ class ModeloController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+
+     public function __construct(Modelo $modelo){
+        $this->modelo = $modelo;
+     }
     public function index()
     {
-        //
+
+         return response()->json($this->modelo->all(), 200);
     }
 
     /**
@@ -35,7 +41,22 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagem/modelos','public');
+
+
+        $modelo = $this->modelo->create([
+            'marca_id'=>$request->marca_id,
+            'nome' =>$request->nome,
+            'imagem' =>$imagem_urn,
+            'numero_portas'=>$request->numero_portas,
+            'lugares'=>$request->lugares,
+            'air_bag'=>$request->air_bag,
+            'abs'=>$request->abs
+        ]);
+        return response()->json($modelo,201);
     }
 
     /**
@@ -44,9 +65,13 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if($modelo === null){
+            return response()->json('Modelo não encontrado', 404);
+        }
+        return response()->json($modelo, 200);
     }
 
     /**
@@ -69,7 +94,48 @@ class ModeloController extends Controller
      */
     public function update(Request $request, Modelo $modelo)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null){
+         return response()->json('Modelo não encontrado', 404);
+        }
+       
+ 
+        if($request->method() === 'PATCH'){
+         
+         $regrasDinamicas = array();
+ 
+         foreach($modelo->rules() as $input => $regra){
+             
+             if(array_key_exists($input, $request->all())) {
+                 $regrasDinamicas[$input] = $regra;
+             }
+ 
+             $request->validate($regrasDinamicas);
+         }
+        }else{
+         $request->validate($modelo->rules());
+        }
+ 
+        if($request->file('imagem')){
+         Storage::disk('public')->delete($modelo->imagem);
+        }
+ 
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagem/modelos', 'public');
+ 
+        $marca->update([
+           'marca_id'=>$request->marca_id,
+            'nome' =>$request->nome,
+            'imagem' =>$imagem_urn,
+            'numero_portas'=>$request->numero_portas,
+            'lugares'=>$request->lugares,
+            'air_bag'=>$request->air_bag,
+            'abs'=>$request->abs
+        ]);
+        
+        $modelo->update($request->all());
+        return $modelo;
     }
 
     /**
@@ -78,8 +144,18 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+   
+        if($modelo == null){
+            return response()->json('Modelo não encontrado', 404);
+        }
+
+        
+            Storage::disk('public')->delete($modelo->imagem);
+           
+        
+        $marca->delete();
     }
 }
